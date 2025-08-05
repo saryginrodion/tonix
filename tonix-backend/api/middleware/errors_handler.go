9508ac3704b080285fc20cdb2f3a082466118fa9
Sarygin Rodion <saryginrodion7@proton.me>
2025-08-05 23:12:16 +0700

    feat: BCKND-2 added sytaxerror and validationerror handlers

--- tonix-backend/api/middleware/errors_handler.go
+++ tonix-backend/api/middleware/errors_handler.go
@@ -1,15 +1,15 @@
 package middleware
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
 	"tonix/backend/api/context"
 	wrap "tonix/backend/api/dto/response_wrapper"
-	"tonix/backend/logging"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/radyshenkya/stackable"
 )
-
-var log = logging.LoggerWithOrigin("mw/errors_handler.go")
 
 var ErrorsHandlerMiddleware = stackable.WrapFunc(
 	func(ctx *context.Context, next func() error) error {
@@ -19,13 +19,32 @@ var ErrorsHandlerMiddleware = stackable.WrapFunc(
 			return nil
 		}
 
+		// Json Parsing error
+		var jsonErr *json.SyntaxError
+		if errors.As(err, &jsonErr) {
+			ctx.Response, _ = stackable.JsonResponse(
+				http.StatusBadRequest,
+				wrap.ErrorsResponse(err.Error(), err.Error()),
+			)
+			return nil
+		}
+
+		// Validation errors
+		if errors.As(err, &validator.ValidationErrors{}) {
+			ctx.Response, _ = stackable.JsonResponse(
+				http.StatusUnprocessableEntity,
+				wrap.ErrorsResponse(err.Error(), err.Error()),
+			)
+			return nil
+		}
+
 		// Generic error handler
 		// Logging generic error
 		ctx.Local.Logger.WithField("origin", "mw/errors_handler.go").WithField("err", err.Error()).Error("Unhandled generic error")
 
 		ctx.Response, _ = stackable.JsonResponse(
 			http.StatusInternalServerError,
-			wrap.ErrorsResponse(err.Error(), []string{err.Error()}),
+			wrap.ErrorsResponse(err.Error(), err.Error()),
 		)
 
 		return nil
